404f1a34c649740e37a37828ff7c7343fcc65ca5
wangzeqing <123@456.com>
2020-04-10 19:01:07 +0800

    add binary tree

--- gotools/binarytree.go
+++ gotools/binarytree.go
@@ -6,8 +6,30 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func CreateBinaryTree([]int) *TreeNode {
-	return nil
+//允许输入数字与nil
+
+func coverCreateBTtoList(str string) []int {
+
+}
+
+func CreateBinaryTree(list []int) (root *TreeNode, err error) {
+	if list == nil {
+		return nil, nil
+	}
+	for _, v := range list {
+		
+	}
+	return
+}
+
+func (tn *TreeNode) add(val int) (isSuccess bool) {
+	if tn == nil {
+		if tn.Left == nil {
+
+		} else if tn.Right == nil {
+
+		}
+	}
 }
 
 //中序
